8c8c005b5b134fbdd9fc44ae7314cf93fad149b9
Diogo <diogoss.dev@gmail.com>
2025-12-21 13:24:37 -0300

    adicionando exercício 22 de python

---
+++ atividades_guanabara/strings/ex22.py
@@ -0,0 +1,15 @@
+#Analisador de Textos
+
+nome = input("Digite seu nome completo: ")
+nome = nome.strip()
+print(f'Seu nome com todas as letras maiúsculas é: {nome.upper()}')
+
+print(f'Seu nome com todas as letras minúsculas é: {nome.lower()}')
+
+comprimento = len(nome)
+espaco = nome.count(' ')
+
+print(f'Seu nome tem {comprimento - espaco} letras.')
+
+nome_list = nome.split()
+print(f'Seu primeiro nome tem {len(nome_list[0])} letras')

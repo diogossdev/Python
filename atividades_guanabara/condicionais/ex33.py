3c518e2dd91a46112a1a9237a9cfc00892a6a125
Diogo <diogoss.dev@gmail.com>
2025-12-24 11:08:15 -0300

    Adicionando exercício 33 do curso.

---
+++ atividades_guanabara/condicionais/ex33.py
@@ -0,0 +1,18 @@
+#Maior e menor valores
+n1 = int(input('Digite o primeiro valor: '))
+n2 = int(input('Digite o segundo valor: '))
+n3 = int(input('Digite o terceiro valor: '))
+
+if n1 > n2 and n1 > n3:
+    print(f'O maior valor digitado foi {n1}')
+elif n2 > n1 and n2 > n3:
+    print(f'O maior valor digitado foi {n2}')
+else:
+    print(f'O maior valor digitado foi {n3}')
+
+if n1 < n2 and n1 < n3:
+    print(f'O menor valor digitado foi {n1}')
+elif n2 < n1 and n2 < n3:
+    print(f'O menor valor digitado foi {n2}')
+else:
+    print(f'O menor valor digitado foi {n3}')
